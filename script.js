994da2546a7027b2d1bb26a929c1eb69a62137f7
Sashait-wq <shevchenoolexandr19@gmail.com>
2025-01-02 17:55:38 +0000

    introduction to javascript

---
+++ script.js
@@ -0,0 +1,43 @@
+// 1 
+
+const name = "Sasha";
+const age = 19;
+
+console.log(name);
+console.log(age);
+
+// 2 
+
+let string1 = "Hello";
+let string2 = "World";
+
+let resultString  = `${string1} ${string2}` ;
+
+console.log(resultString);
+
+// 3 
+
+let num1 = 20;
+let num2 = 2;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+// 4
+
+let strNum = "5";
+
+let convertedNum = Number(strNum);
+console.log(convertedNum);
+
+// 5 
+
+let strValue = "Hello";
+let booleanStr = Boolean(strValue);
+console.log(booleanStr);
+
+let numValue = 0;
+let booleanNum = Boolean(numValue);
+console.log(booleanNum); 
